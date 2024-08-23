ed1ef0fb406e80283dcf17a71539acc5853e1b53
drendev <91354016+drendev@users.noreply.github.com>
2024-08-23 18:15:53 +0800

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+
+const ibm = IBM_Plex_Sans({ subsets: ["latin"], weight: "300",  });
+
+export const metadata: Metadata ={
+  title: "iTaskDev",
+  description: "Optimize Task Assignment in Software Development",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={ibm.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-center">
+        Hello World, iTaskDev is a Task Software Development with a collaborative workspace
+      </div>
+    </>
+  );
+}
