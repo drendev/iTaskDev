eac7ddd814b359e6476931ca8eee8d5a09e10290
drendev <91354016+drendev@users.noreply.github.com>
2024-08-24 15:48:49 +0800

    Authentication

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,14 @@
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import type { NextAuthConfig } from "next-auth"
 
-export default { providers: [GitHub] } satisfies NextAuthConfig
+export default { providers: [
+    GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })]
+} satisfies NextAuthConfig
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { signIn } from "next-auth/react";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import {DEFAULT_LOGIN_REDIRECT} from "@/route";
 
 export const LoginForm = () => {
+    const onClick = (provider: "google" | "github") => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT
+        });
+    }
+
     return(
         <CardWrapper
         headerLabel="Login to iTaskDev"
@@ -13,6 +23,7 @@ export const LoginForm = () => {
                 <Button
                 className="w-full text-md font-semibold"
                 size="lg"
+                onClick={() => onClick("github")}
                 >
                     <FaGithub className="mr-2"/> Sign In with Github
                 </Button>
@@ -23,6 +34,7 @@ export const LoginForm = () => {
                     className="w-full text-md font-semibold"
                     variant="outline"
                     size="lg"
+                    onClick={() => onClick("google")}
                 >
                     <FcGoogle className="mr-2"/> Sign In with Google
                 </Button>
