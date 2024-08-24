126103ecf96cead35d1675d087ef5dc4b8c62af9
drendev <91354016+drendev@users.noreply.github.com>
2024-08-24 11:51:51 +0800

    Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="text-center">
-        Hello World, iTaskDev is a Task Software Development with a collaborative workspace
-      </div>
+      <main className="flex h-full flex-col items-center justify-center">
+        <div className="space-y-6 text-center">
+            <h1 className="text-2xl font-semibold text-slate-800">
+                iTaskDev
+            </h1>
+            <p className="text-lg">
+               Under Development
+            </p>
+            <div>
+                <Link href={"/auth/login"} className="text-slate-900 hover:text-slate-600 font-bold">
+                    Sign In
+                </Link>
+            </div>
+        </div>
+      </main>
     </>
   );
 }
