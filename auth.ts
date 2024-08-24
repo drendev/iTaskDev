3a082ab97cb312eca0d43aa6125dd846512783b9
drendev <91354016+drendev@users.noreply.github.com>
2024-08-24 11:32:59 +0800

    Authentication

---
+++ auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import authConfig from "@/auth.config";
+import { PrismaAdapter} from "@auth/prisma-adapter";
+import { db } from "@/lib/db"
+
+export const { auth, handlers: { GET, POST}, signIn, signOut } = NextAuth({
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt"},
+    ...authConfig
+})
---
+++ route.ts
@@ -0,0 +1,15 @@
+// An array of routes that are public, these routes don't need authentications
+export const publicRoutes = [
+    "/"
+];
+
+// An array of routes for authentication
+export const authRoutes = [
+    "/auth/login",
+];
+
+// the prefix for authentication routes
+export const apiAuthPrefix = "/api/auth";
+
+// Redirect page after login
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
