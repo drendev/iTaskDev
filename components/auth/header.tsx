fa03aadaec03412772666906311c345917e41baa
drendev <91354016+drendev@users.noreply.github.com>
2024-08-25 11:03:35 +0800

    Authentication

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -7,7 +7,7 @@ export const Header = ({ label }: HeaderProps) => {
     return (
         <div className="w-full flex flex-col gap-y-4 items-center">
             <h1 className="text-3xl font-semibold">
-                Sign In
+                Sign in to your account
             </h1>
             <p className="text-muted-foreground text-sm">
                 {label}
