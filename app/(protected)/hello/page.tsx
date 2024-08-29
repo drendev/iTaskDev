988ed652d0dbb43959c02c2952b6f075a55b5f85
hiromutech <148634375+hiromutech@users.noreply.github.com>
2024-08-29 19:57:56 +0800

    Tentative Design Landing Page

--- app/(protected)/hello/page.tsx
+++ app/(protected)/hello/page.tsx
@@ -1,23 +1,28 @@
 "use client";
 
-
-import {logout} from "@/actions/logout";
-import {useCurrentUser} from "@/hooks/use-current-user";
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import {Navbar} from "@/app/(protected)/_components/navbar";
+import { Hero } from "@/app/(protected)/_components/hero";
 
 const SettingsPage = () => {
-    const session = useCurrentUser()
+  const session = useCurrentUser();
 
-    const onClick = () => {
-        logout();
-    }
+  const onClick = () => {
+    logout();
+  };
 
-    return (
-        <div>
-            <button type="submit" onClick={onClick}>
-                Sign Out
-            </button>
-        </div>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <div>
+        <button type="submit" onClick={onClick}>
+          Sign Out
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default SettingsPage;
--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,15 +1,12 @@
-import {Navbar} from "@/app/(protected)/_components/navbar";
-
 interface ProtectedLayoutProps {
     children: React.ReactNode;
 }
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
     return(
-        <div className="h-full w-full flex flex-col gap-y-10">
-            <Navbar />
-            {children}
-        </div>
+            <div className="antialiased bg-[#EAEEFE]">
+                {children}
+            </div>
     )
 }
 
