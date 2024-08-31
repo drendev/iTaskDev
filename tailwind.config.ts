21660dcb284c5bb45e27d8d5a9e67e7f1e9841fd
hiromutech <148634375+hiromutech@users.noreply.github.com>
2024-08-31 21:12:40 +0800

    Sign-In Page Front End 60%

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -76,5 +79,7 @@ const config = {
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
+
+
 
 export default config
