3397b96da9dc7a6fe313f86851cbc45635a69e38
Charles Sherwin San Gabriel <148634375+hiromutech@users.noreply.github.com>
2024-09-01 15:18:50 +0800

    DROPDOWN MO BIUTCH

--- auth.ts
+++ auth.ts
@@ -1,38 +1,38 @@
-import NextAuth, { DefaultSession } from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth";
 import authConfig from "@/auth.config";
-import { getUserById} from "@/data/user";
-import { PrismaAdapter} from "@auth/prisma-adapter";
-import { db } from "@/lib/db"
+import { getUserById } from "@/data/user";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    pages: {
-        signIn: "/auth/login",
-        error: "/auth/error"
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
     },
-    callbacks: {
-        async session({ token, session }) {
-            if (token.sub && session.user) {
-                session.user.id = token.sub;
-            }
+    async jwt({ token }) {
+      if (!token.sub) return token;
 
-            if (token.role && session.user) {
-                session.user.role = token.role;
-            }
-            return session
-        },
-        async jwt({ token }){
-            if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
 
-            const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
 
-            if (!existingUser) return token;
+      token.role = existingUser.role;
 
-            token.role = existingUser.role;
-
-            return token;
-        }
+      return token;
     },
-    adapter: PrismaAdapter(db),
-    session: { strategy: "jwt"},
-    ...authConfig
-})
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
