93a09e2edb95e89dcb0577d3c745eb6319ef7dd4
drendev <91354016+drendev@users.noreply.github.com>
2024-09-03 13:46:43 +0800

    Dashboard

--- app/(protected)/_components/navbar/nav-utils.tsx
+++ app/(protected)/_components/navbar/nav-utils.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import Image from "next/image";
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { GoBell } from "react-icons/go";
 import { GoInbox } from "react-icons/go";
 import { GoVersions } from "react-icons/go";
 import { GoGoal } from "react-icons/go";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { FaUser } from "react-icons/fa";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {
+    Avatar,
+    AvatarImage,
+    AvatarFallback
+} from "@/components/ui/avatar";
+import { LogoutButton } from "@/components/auth/logout-button";
 
 export const NavUtils = () => {
-    const session = useCurrentUser();
+    const user = useCurrentUser();
 
     return (
         <div className="flex justify-end items-center">
@@ -22,13 +34,26 @@ export const NavUtils = () => {
             <Button variant="ghost" className="rounded-full">
                 <GoGoal className="w-5 h-5" color="#475569"/>
             </Button>
-            <Button variant="ghost" className="rounded-full">
+            <Button variant="ghost" className="rounded-full mr-3.5">
             <GoVersions className="w-5 h-5" color="#475569"/>
             </Button>
-
-            <div className="rounded-full hover:bg-slate-100 p-1 cursor-pointer">
-
-            </div>
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar>
+                        <AvatarImage height={28} width={28} src={user?.image || ""} />
+                            <AvatarFallback>
+                                <FaUser />
+                            </AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-40" align="end">
+                    <LogoutButton>
+                        <DropdownMenuItem>
+                            Logout
+                        </DropdownMenuItem>
+                    </LogoutButton>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     )
 }
--- app/(protected)/hello/page.tsx
+++ app/(protected)/hello/page.tsx
@@ -1,21 +1,14 @@
 "use client";
 
-import { logout } from "@/actions/logout";
 import { useCurrentUser } from "@/hooks/use-current-user";
 
 const SettingsPage = () => {
   const session = useCurrentUser();
 
-  const onClick = () => {
-    logout();
-  };
-
   return (
     <>
       <div>
-        <button type="submit" onClick={onClick}>
-          Sign Out
-        </button>
+
       </div>
     </>
   );
