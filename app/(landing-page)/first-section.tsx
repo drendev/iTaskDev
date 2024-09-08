319b6d48572ad4cc287aa0c78d99bda2d1aff8a7
Charles Sherwin San Gabriel <148634375+hiromutech@users.noreply.github.com>
2024-09-08 11:41:44 +0800

    Mas magulong code para sayo ya!

--- app/(landing-page)/first-section.tsx
+++ app/(landing-page)/first-section.tsx
@@ -7,6 +7,9 @@ import {
   PiListChecks,
   PiNotebook,
   PiCode,
+  PiHeadset,
+  PiCalendarDotsLight,
+  PiSparkleLight,
 } from "react-icons/pi";
 
 import { useMediaQuery } from "react-responsive";
@@ -27,39 +30,24 @@ const sections = [
     description:
       "Capture ideas, gather feedback from teammates, and ask AI to add the finishing touches.",
     image: "/samples/iso1.png",
-    fig1: (
-      <div>
-        <PiLaptopLight className="w-20 h-20" />
-        <p className="text-sm">Building blocks</p>
-        <div className="w-32 text-xs text-gray-400">
-          100+ content types to communicate any idea.
-        </div>
-      </div>
-    ),
-    fig2: (
-      <div>
-        <PiListChecks className="w-20 h-20" />
-        <p className="text-sm">Collaborative tools</p>
-        <div className="w-32 text-xs text-gray-400">
-          Built for teams to share, suggest, and comment.
-        </div>
-      </div>
-    ),
-    fig3: (
-      <div>
-        <PiNotebook className="w-20 h-20" />
-        <p className="text-sm">AI-assisted</p>
-        <div className="w-32 text-xs text-gray-400">
-          Edit, draft, translate. Ask, and AI will answer.
-        </div>
-      </div>
-    ),
+    fig1img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig1imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig1title: "Building blocks",
+    fig1desc: "100+ content types to communicate any idea.",
+    fig2img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig2imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig2title: "Building blocks",
+    fig2desc: "100+ content types to communicate any idea.",
+    fig3img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig3imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig3title: "Building blocks",
+    fig3desc: "100+ content types to communicate any idea.",
   },
   {
     tag: (
-      <div className="bg-blue-200 flex items-center text-blue-800 gap-2 p-2 rounded-xl text-sm">
-        <PiCode className="w-5 h-5" />
-        Projects
+      <div className="bg-red-200 flex items-center text-red-800 gap-2 p-2 rounded-xl text-sm">
+        <PiHeadset className="w-5 h-5" />
+        Communication
       </div>
     ),
     header: (
@@ -70,33 +58,74 @@ const sections = [
     description:
       "Capture ideas, gather feedback from teammates, and ask AI to add the finishing touches.",
     image: "/samples/iso1.png",
-    fig1: (
-      <div>
-        <PiLaptopLight className="w-20 h-20" />
-        <p className="text-sm">Building blocks</p>
-        <div className="w-32 text-xs text-gray-400">
-          100+ content types to communicate any idea.
-        </div>
+    fig1img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig1imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig1title: "Building blocks",
+    fig1desc: "100+ content types to communicate any idea.",
+    fig2img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig2imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig2title: "Building blocks",
+    fig2desc: "100+ content types to communicate any idea.",
+    fig3img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig3imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig3title: "Building blocks",
+    fig3desc: "100+ content types to communicate any idea.",
+  },
+  {
+    tag: (
+      <div className="bg-indigo-200 flex items-center text-indigo-800 gap-2 p-2 rounded-xl text-sm">
+        <PiCalendarDotsLight className="w-5 h-5" />
+        Calendar
       </div>
     ),
-    fig2: (
-      <div>
-        <PiListChecks className="w-20 h-20" />
-        <p className="text-sm">Collaborative tools</p>
-        <div className="w-32 text-xs text-gray-400">
-          Built for teams to share, suggest, and comment.
-        </div>
+    header: (
+      <div className="text-6xl">
+        Plan your <br /> next endeavor.
       </div>
     ),
-    fig3: (
-      <div>
-        <PiNotebook className="w-20 h-20" />
-        <p className="text-sm">AI-assisted</p>
-        <div className="w-32 text-xs text-gray-400">
-          Edit, draft, translate. Ask, and AI will answer.
-        </div>
+    description:
+      "Capture ideas, gather feedback from teammates, and ask AI to add the finishing touches.",
+    image: "/samples/iso1.png",
+    fig1img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig1imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig1title: "Building blocks",
+    fig1desc: "100+ content types to communicate any idea.",
+    fig2img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig2imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig2title: "Building blocks",
+    fig2desc: "100+ content types to communicate any idea.",
+    fig3img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig3imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig3title: "Building blocks",
+    fig3desc: "100+ content types to communicate any idea.",
+  },
+  {
+    tag: (
+      <div className="bg-purple-200 flex items-center text-purple-800 gap-2 p-2 rounded-xl text-sm">
+        <PiSparkleLight className="w-5 h-5" />
+        AI
       </div>
     ),
+    header: (
+      <div className="text-6xl">
+        Plan your <br /> next endeavor.
+      </div>
+    ),
+    description:
+      "Capture ideas, gather feedback from teammates, and ask AI to add the finishing touches.",
+    image: "/samples/iso1.png",
+    fig1img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig1imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig1title: "Building blocks",
+    fig1desc: "100+ content types to communicate any idea.",
+    fig2img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig2imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig2title: "Building blocks",
+    fig2desc: "100+ content types to communicate any idea.",
+    fig3img: <PiLaptopLight className="w-16 h-16 mr-5" />,
+    fig3imglg: <PiLaptopLight className="w-20 h-20 mr-5" />,
+    fig3title: "Building blocks",
+    fig3desc: "100+ content types to communicate any idea.",
   },
 ];
 
@@ -105,109 +134,116 @@ const FirstSection = () => {
 
   return (
     <>
-      {isSmallScreen ? (
-        sections.map((section) => (
-          <div className="flex flex-col px-8">
-            <div className="flex mt-20 mb-5">{section.tag}</div>
-            {section.header}
-            <div className="mt-3 text-gray-400 text-lg">
-              {section.description}
-            </div>
-            <div
-              className="py-10 lg:w-[80%]
-        2xl:w-[55%]"
-            >
-              <div className="md:flex items-center justify-center space-x-6 hover:cursor-pointer w-full">
-                <Image
-                  src={section.image}
-                  alt="image"
-                  width={1000}
-                  height={1000}
-                  className="w-full border rounded-xl shadow-md"
-                />
-              </div>
-            </div>
-            {/* Three figures */}
-            <div className="flex space-x-20 mx-auto h-56">
-              {section.fig1}
-              {section.fig2}
-              {section.fig3}
-            </div>
-          </div>
-        ))
-      ) : (
-        <div className="items-center flex flex-col mt-5">
-          <div
-            className="flex xl:space-x- items-center justify-between gap-4 py-10 px-8 lg:w-[80%]
-        2xl:w-[55%]"
-          >
-            <div className="w-[50%]">
-              <div className="flex mb-5">
-                <div className="bg-blue-200 flex items-center text-blue-800 gap-2 p-2 rounded-xl text-sm">
-                  <PiCode className="w-5 h-5" />
-                  Projects
-                </div>
-              </div>
-              <div className="text-6xl">
-                Plan your <br />
-                next endeavor.
-              </div>
+      {isSmallScreen
+        ? sections.map((section) => (
+            <div className="flex flex-col px-8">
+              <div className="flex mt-20 mb-5">{section.tag}</div>
+              {section.header}
               <div className="mt-3 text-gray-400 text-lg">
-                Capture ideas, gather feedback from teammates, and ask AI to add
-                the finishing touches.
+                {section.description}
               </div>
-            </div>
-
-            <div className="flex space-x-5 mx-auto h-56">
-              <div>
-                <br></br>
-                <br></br>
-                <br></br>
-                <PiLaptopLight className="w-20 h-20" />
-                <p className="text-sm">Building blocks</p>
-                <div className="w-32 text-xs text-gray-400">
-                  100+ content types to communicate any idea.
-                </div>
-              </div>
-              <div>
-                <br></br>
-                <br></br>
-                <br></br>
-                <PiListChecks className="w-20 h-20" />
-                <p className="text-sm">Collaborative tools</p>
-                <div className="w-32 text-xs text-gray-400">
-                  Built for teams to share, suggest, and comment.
-                </div>
-              </div>
-              <div>
-                <br></br>
-                <br></br>
-                <br></br>
-                <PiNotebook className="w-20 h-20" />
-                <p className="text-sm">AI-assisted</p>
-                <div className="w-32 text-xs text-gray-400">
-                  Edit, draft, translate. Ask, and AI will answer.
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className="py-10 px-8 lg:w-[80%]
+              <div
+                className="py-10 lg:w-[80%]
         2xl:w-[55%]"
-          >
-            <div className="md:flex items-center justify-center space-x-6 hover:cursor-pointer w-full">
-              <Image
-                src="/samples/iso1.png"
-                alt="bueno"
-                width={1000}
-                height={1000}
-                className="w-full border rounded-xl shadow-md"
-              />
+              >
+                <div className="md:flex items-center justify-center space-x-6 hover:cursor-pointer w-full">
+                  <Image
+                    src={section.image}
+                    alt="image"
+                    width={1000}
+                    height={1000}
+                    className="w-full border rounded-xl shadow-md"
+                  />
+                </div>
+              </div>
+              {/* Three figures */}
+              <div className="flex items-center mb-5">
+                {section.fig1img}
+                <div>
+                  <p className="text-lg">{section.fig1title}</p>
+                  <div className="text-sm text-gray-400">
+                    {section.fig1desc}
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center mb-5">
+                {section.fig2img}
+                <div>
+                  <p className="text-lg">{section.fig2title}</p>
+                  <div className="text-sm text-gray-400">
+                    {section.fig2desc}
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center mb-5">
+                {section.fig3img}
+                <div>
+                  <p className="text-lg">{section.fig3title}</p>
+                  <div className="text-sm text-gray-400">
+                    {section.fig3desc}
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          ))
+        : sections.map((section) => (
+            <div className="items-center flex flex-col mt-5">
+              <div
+                className="flex xl:space-x- items-center justify-between gap-4 py-10 px-8 lg:w-[80%]
+        2xl:w-[55%]"
+              >
+                <div className="w-[50%]">
+                  <div className="flex mb-5">{section.tag}</div>
+                  {section.header}
+                  <div className="mt-3 text-gray-400 text-lg">
+                    {section.description}
+                  </div>
+                </div>
+
+                <div className="flex space-x-5 mx-auto h-56">
+                  <div>
+                    <div className="mb-20"></div>
+                    {section.fig1imglg}
+                    <p className="text-sm">{section.fig1title}</p>
+                    <div className="w-32 text-xs text-gray-400">
+                      {section.fig1desc}
+                    </div>
+                  </div>
+                  <div>
+                    <div className="mb-20"></div>
+                    {section.fig2imglg}
+                    <p className="text-sm">{section.fig2title}</p>
+                    <div className="w-32 text-xs text-gray-400">
+                      {section.fig2desc}
+                    </div>
+                  </div>
+                  <div>
+                    <div className="mb-20"></div>
+                    {section.fig3imglg}
+                    <p className="text-sm">{section.fig3title}</p>
+                    <div className="w-32 text-xs text-gray-400">
+                      {section.fig3desc}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="py-10 px-8 lg:w-[80%]
+        2xl:w-[55%]"
+              >
+                <div className="md:flex items-center justify-center space-x-6 hover:cursor-pointer w-full">
+                  <Image
+                    src="/samples/iso1.png"
+                    alt="bueno"
+                    width={1000}
+                    height={1000}
+                    className="w-full border rounded-xl shadow-md"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
     </>
   );
 };
