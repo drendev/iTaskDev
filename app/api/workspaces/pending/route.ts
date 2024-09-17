11e872d2ec2c17ca15873e4e0124f466ea987d87
drendev <91354016+drendev@users.noreply.github.com>
2024-09-17 20:38:18 +0800

    Pending api

---
+++ app/api/workspaces/pending/route.ts
@@ -0,0 +1,36 @@
+import { currentUser } from "@/lib/auth"
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server"
+import { v4 as uuidv4 } from 'uuid';
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { workspacesId: string } }
+) {
+    try {
+        const user = await currentUser();
+
+        if (!user) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!params.workspacesId) {
+            return new NextResponse("Not Found", { status: 400 });
+        }
+
+        const project = await db.workspace.update({
+            where: {
+                id: params.workspacesId,
+                userId: user.id
+            },
+            data: {
+                isPrivate: false
+            }
+        })
+
+        return NextResponse.json(project);
+    } catch (error) {
+        console.log("[WORKSPACE_ID]", error)
+        return new NextResponse("Internal Server Error", { status: 500 })
+    }
+}
