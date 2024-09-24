ff44689dc200f1efc2f18b12b838fe9e1c9e41b1
Charles Sherwin San Gabriel <148634375+hiromutech@users.noreply.github.com>
2024-09-24 12:56:51 +0800

    sdlc reco

--- app/(main)/(routes)/home/page.tsx
+++ app/(main)/(routes)/home/page.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -9,15 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+
 import { Separator } from "@/components/ui/separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Image from "next/image";
