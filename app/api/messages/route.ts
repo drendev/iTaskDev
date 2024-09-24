770cd1a40ba40c4d77bec4a6484e0af982a8acf1
drendev <91354016+drendev@users.noreply.github.com>
2024-09-24 21:21:38 +0800

    Update route.ts

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { ProjectChat } from "@prisma/client";
 
-const MESSAGES_BATCH = 5;
+const MESSAGES_BATCH = 6;
 
 export async function GET(
     req: Request
