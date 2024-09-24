69d42a02428786946f39cb8e07c0404c30c5682b
drendev <91354016+drendev@users.noreply.github.com>
2024-09-24 15:13:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-      domains: [
-        "uploadthing.com",
-        "utfs.io"
-      ]
-    }
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil"
+    });
+
+    return config;
+  },
+  images: {
+    domains: [
+      "uploadthing.com",
+      "utfs.io"
+    ]
   }
-  
-  module.exports = nextConfig
-  
+}
+
+module.exports = nextConfig
