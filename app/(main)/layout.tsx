7de1b8e9774ec6123c0e9974dce0c100a34f0f02
Charles Sherwin San Gabriel <148634375+hiromutech@users.noreply.github.com>
2024-09-27 22:13:28 +0800

    awa nalang

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,18 +1,21 @@
 import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
 import ToastHandler from "../(invite)/(routes)/invite/_components/toast-handler";
 import { Modals } from "@/components/providers/modal-provider";
+import { ContentLayout } from "@/components/admin-panel/content-layout";
 
 export default function DemoLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (<div>
-    <AdminPanelLayout>
+  return (
+    <div>
+      <AdminPanelLayout>
         <ToastHandler />
         <Modals />
-        {children}
-    </AdminPanelLayout>;
-  </div>
-);
+        <ContentLayout>{children}</ContentLayout>
+      </AdminPanelLayout>
+      ;
+    </div>
+  );
 }
--- components/projects/main/projectlist.tsx
+++ components/projects/main/projectlist.tsx
@@ -10,6 +10,8 @@ import {
   Cherry,
   Cloudy,
   Ellipsis,
+  TestTube,
+  Rocket,
 } from "lucide-react";
 import {
   Table,
@@ -31,15 +33,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+import { useMediaQuery } from "react-responsive";
 
 const test = [
   {
@@ -92,33 +97,127 @@ const test = [
     SDLC: "DevOps",
     openTest: "10 days ago",
   },
+
+  {
+    project: (
+      <Link href="#" className="hover:underline flex items-center gap-3">
+        <Cloudy className="p-2 rounded-lg bg-fuchsia-500 w-10 h-10 text-white" />
+        iCloud
+      </Link>
+    ),
+    SDLC: "DevOps",
+    openTest: "10 days ago",
+  },
+  {
+    project: (
+      <Link href="#" className="hover:underline flex items-center gap-3">
+        <Cloudy className="p-2 rounded-lg bg-fuchsia-500 w-10 h-10 text-white" />
+        iCloud
+      </Link>
+    ),
+    SDLC: "DevOps",
+    openTest: "10 days ago",
+  },
+  {
+    project: (
+      <Link href="#" className="hover:underline flex items-center gap-3">
+        <Cloudy className="p-2 rounded-lg bg-fuchsia-500 w-10 h-10 text-white" />
+        iCloud
+      </Link>
+    ),
+    SDLC: "DevOps",
+    openTest: "10 days ago",
+  },
+  {
+    project: (
+      <Link href="#" className="hover:underline flex items-center gap-3">
+        <Cloudy className="p-2 rounded-lg bg-fuchsia-500 w-10 h-10 text-white" />
+        iCloud
+      </Link>
+    ),
+    SDLC: "DevOps",
+    openTest: "10 days ago",
+  },
+  {
+    project: (
+      <Link href="#" className="hover:underline flex items-center gap-3">
+        <Cloudy className="p-2 rounded-lg bg-fuchsia-500 w-10 h-10 text-white" />
+        iCloud
+      </Link>
+    ),
+    SDLC: "DevOps",
+    openTest: "10 days ago",
+  },
 ];
 
 const ProjectList = () => {
   const [layout, setLayout] = useState<string>("list");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(5);
+  const isSmallScreen = useMediaQuery({ maxWidth: 767 });
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = test.slice(firstPostIndex, lastPostIndex);
+
+  console.log(currentPosts);
   return (
     <>
-      <div className="text-xl font-semibold ml-4">Projects</div>
-      <div className="mt-5 flex justify-between">
-        <div>
-          <Button variant="ghost">
-            Opened by you
-            <ChevronDown className="ml-3" />
-          </Button>
-          <Button variant="ghost">
-            Ascending
-            <ChevronDown className="ml-3" />
-          </Button>
+      {isSmallScreen ? (
+        <div className="mt-5 flex flex-col justify-between my-5">
+          <div className="flex items-center gap-3 text-4xl font-semibold ml-4">
+            <Rocket size={30} />
+            Projects
+          </div>
+
+          <div className="flex space-x-2 mt-5 justify-between">
+            <Button>+ Create a Project</Button>
+            <div>
+              <Button
+                variant="ghost"
+                onClick={() => (setLayout("list"), setPostsPerPage(5))}
+                className={layout === "list" ? "bg-gray-100" : ""}
+              >
+                <AlignJustify />
+              </Button>
+              <Button
+                variant="ghost"
+                onClick={() => (setLayout("grid"), setPostsPerPage(8))}
+                className={layout === "grid" ? "bg-gray-100" : ""}
+              >
+                <Grid2X2 />
+              </Button>
+            </div>
+          </div>
         </div>
-        <div className="flex space-x-2">
-          <Button variant="ghost" onClick={() => setLayout("list")}>
-            <AlignJustify />
-          </Button>
-          <Button variant="ghost" onClick={() => setLayout("grid")}>
-            <Grid2X2 />
-          </Button>
+      ) : (
+        <div className="mt-5 flex justify-between my-5">
+          <div className="flex items-center gap-3 text-4xl font-semibold ml-4">
+            <Rocket size={30} />
+            Projects
+          </div>
+
+          <div className="flex space-x-2 mt-5 justify-between">
+            <Button>+ Create a Project</Button>
+            <div>
+              <Button
+                variant="ghost"
+                onClick={() => (setLayout("list"), setPostsPerPage(5))}
+                className={layout === "list" ? "bg-gray-100" : ""}
+              >
+                <AlignJustify />
+              </Button>
+              <Button
+                variant="ghost"
+                onClick={() => (setLayout("grid"), setPostsPerPage(8))}
+                className={layout === "grid" ? "bg-gray-100" : ""}
+              >
+                <Grid2X2 />
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
 
       {layout === "list" ? (
         <div>
@@ -127,35 +226,27 @@ const ProjectList = () => {
               <TableRow>
                 <TableHead>Name</TableHead>
                 <TableHead>SDLC</TableHead>
-                <TableHead>Opened by you</TableHead>
+                {!isSmallScreen && <TableHead>Opened by you</TableHead>}
               </TableRow>
             </TableHeader>
             <TableBody>
-              {test.map((proj, index) => (
+              {currentPosts.map((proj, index) => (
                 <TableRow key={index}>
                   <TableCell className="font-medium">{proj.project}</TableCell>
                   <TableCell>{proj.SDLC}</TableCell>
-                  <TableCell>{proj.openTest}</TableCell>
-                  <TableCell>
-                    <Button variant="ghost">
-                      <Ellipsis />
-                    </Button>
-                  </TableCell>
+                  {!isSmallScreen && <TableCell>{proj.openTest}</TableCell>}
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </div>
       ) : (
-        <div className="grid grid-cols-4 gap-2 mt-4">
-          {test.map((proj, index) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-4">
+          {currentPosts.map((proj, index) => (
             <Card className="mb-5 mr-5" key={index}>
               <CardHeader>
                 <CardTitle className="flex justify-between items-center text-lg">
                   {proj.project}
-                  <Button variant="ghost">
-                    <Ellipsis className="w-5 h-5" />
-                  </Button>
                 </CardTitle>
               </CardHeader>
               <CardContent>
@@ -166,8 +257,107 @@ const ProjectList = () => {
           ))}
         </div>
       )}
+      <PaginationSection
+        totalPosts={test.length}
+        postsPerPage={postsPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </>
   );
 };
 
 export default ProjectList;
+
+function PaginationSection({
+  totalPosts,
+  postsPerPage,
+  currentPage,
+  setCurrentPage,
+}: {
+  totalPosts: any;
+  postsPerPage: any;
+  currentPage: any;
+  setCurrentPage: any;
+}) {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const maxPageNum = 5; // Maximum page numbers to display at once
+  const pageNumLimit = Math.floor(maxPageNum / 2); // Current page should be in the middle if possible
+
+  let activePages = pageNumbers.slice(
+    Math.max(0, currentPage - 1 - pageNumLimit),
+    Math.min(currentPage - 1 + pageNumLimit + 1, pageNumbers.length)
+  );
+
+  const handleNextPage = () => {
+    if (currentPage < pageNumbers.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  // Function to render page numbers with ellipsis
+  const renderPages = () => {
+    const renderedPages = activePages.map((page, idx) => (
+      <PaginationItem
+        key={idx}
+        className={currentPage === page ? "bg-neutral-100 rounded-md" : ""}
+      >
+        <PaginationLink onClick={() => setCurrentPage(page)}>
+          {page}
+        </PaginationLink>
+      </PaginationItem>
+    ));
+
+    // Add ellipsis at the start if necessary
+    if (activePages[0] > 1) {
+      renderedPages.unshift(
+        <PaginationEllipsis
+          key="ellipsis-start"
+          onClick={() => setCurrentPage(activePages[0] - 1)}
+        />
+      );
+    }
+
+    // Add ellipsis at the end if necessary
+    if (activePages[activePages.length - 1] < pageNumbers.length) {
+      renderedPages.push(
+        <PaginationEllipsis
+          key="ellipsis-end"
+          onClick={() =>
+            setCurrentPage(activePages[activePages.length - 1] + 1)
+          }
+        />
+      );
+    }
+
+    return renderedPages;
+  };
+
+  return (
+    <div className="my-10">
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious onClick={handlePrevPage} />
+          </PaginationItem>
+
+          {renderPages()}
+
+          <PaginationItem>
+            <PaginationNext onClick={handleNextPage} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+}
